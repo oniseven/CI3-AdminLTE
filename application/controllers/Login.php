69e95ef90648eb81a755b4ad6e61aff5ed9157a6
oniseven <lukman08119@gmail.com>
2024-01-24 12:53:50 +0700

    Adding Login Page

---
+++ application/controllers/Login.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+	public function index()
+	{
+		$this->template
+      ->page_type('blank')
+      ->page_title('Login page')
+      ->set_class('body', 'hold-transition login-page')
+      ->load('login');
+	}
+}
