5b998d19488a4864d6e0d6b419f57463f5b2dae3
oniseven <lukman08119@gmail.com>
2024-01-24 12:57:54 +0700

    Blank page structures

---
+++ application/views/template/blank/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <base href="<?php echo base_url(); ?>" />
+  <title><?php echo $page_title; ?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
+</head>
+<body class="<?php echo $classes['body']; ?>">
