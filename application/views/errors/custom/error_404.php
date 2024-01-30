3835616fccaf192ebdb0f0fe28a967dbbb4ece50
oniseven <lukman08119@gmail.com>
2024-01-30 10:33:51 +0700

    Adding flex and centered item on upper div

--- application/views/errors/custom/error_404.php
+++ application/views/errors/custom/error_404.php
@@ -1,26 +1,28 @@
-<div class="error-page">
-  <h2 class="headline text-warning"> 404</h2>
+<div class="flex align-items-center">
+  <div class="error-page">
+    <h2 class="headline text-warning"> 404</h2>
 
-  <div class="error-content">
-    <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
+    <div class="error-content">
+      <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h3>
 
-    <p>
-      We could not find the page you were looking for.
-      Meanwhile, you may <a href="<?php echo base_url(); ?>">return to dashboard</a> or try using the search form.
-    </p>
+      <p>
+        We could not find the page you were looking for.
+        Meanwhile, you may <a href="<?php echo base_url(); ?>">return to dashboard</a> or try using the search form.
+      </p>
 
-    <form class="search-form">
-      <div class="input-group">
-        <input type="text" name="search" class="form-control" placeholder="Search">
+      <form class="search-form">
+        <div class="input-group">
+          <input type="text" name="search" class="form-control" placeholder="Search">
 
-        <div class="input-group-append">
-          <button type="submit" name="submit" class="btn btn-warning"><i class="fas fa-search"></i>
-          </button>
+          <div class="input-group-append">
+            <button type="submit" name="submit" class="btn btn-warning"><i class="fas fa-search"></i>
+            </button>
+          </div>
         </div>
-      </div>
-      <!-- /.input-group -->
-    </form>
+        <!-- /.input-group -->
+      </form>
+    </div>
+    <!-- /.error-content -->
   </div>
-  <!-- /.error-content -->
-</div>
-<!-- /.error-page -->
+  <!-- /.error-page -->
+</div>  
