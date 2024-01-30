094ffd1d03518fdd824924fe4cd4f5dbed7a5572
oniseven <lukman08119@gmail.com>
2024-01-30 10:56:15 +0700

    Adding custom page js and page css links on header and footer

--- application/views/template/blank/footer.php
+++ application/views/template/blank/footer.php
@@ -1,2 +1,27 @@
+  <!-- REQUIRED SCRIPTS -->
+  <script>
+    const pageUrl = '<?=current_url()?>'
+  </script>
+  <!-- jQuery -->
+  <script src="assets/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Vendors App -->
+  <!-- Sweet Alert 2 -->
+  <script src="assets/plugins/sweetalert2/sweetalert2.min.js"></script>
+  <?php
+    foreach ($plugin_js as $key => $value) {
+      echo sprintf('<script src="%s"></script>', $value);
+    }
+  ?>
+  <!-- AdminLTE App -->
+  <script src="assets/dist/js/adminlte.min.js"></script>
+  <script src="assets/dist/js/cignadlte.js"></script>
+  <!-- Current Custom Page JS -->
+  <?php
+    foreach ($page_js as $key => $value) {
+      echo sprintf('<script src="%s"></script>', $value);
+    }
+  ?>
 </body>
 </html>
--- application/views/template/blank/header.php
+++ application/views/template/blank/header.php
@@ -10,6 +10,14 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+  <!-- Vendors style -->
+  <!-- Sweet Alert 2 -->
+  <link rel="stylesheet" href="assets/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <?php
+    foreach ($plugin_css as $key => $value) {
+      echo sprintf('<link rel="stylesheet" type="text/css" href="%s" />', $value);
+    }
+  ?>
   <!-- Theme style -->
   <link rel="stylesheet" href="assets/dist/css/adminlte.min.css">
   <!-- Custom style -->
